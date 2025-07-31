ff96b2b8f6e2c06067f1c865cc70c34cb84a3a9c
Nguyễn Văn Kiên <anhkien7780@gmail.com>
2025-08-01 01:23:14 +0700

    feat: picture preview and take picture

---
+++ lib/ui/take_picture_screen.dart
@@ -0,0 +1,64 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:take_picture_flutter/ui/widget/display_picture_screen.dart';
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({super.key, required this.camera});
+
+  final CameraDescription camera;
+
+  @override
+  State<TakePictureScreen> createState() => _TakePictureScreenState();
+}
+
+class _TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(widget.camera, ResolutionPreset.medium);
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            if (!context.mounted) return;
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) =>
+                    DisplayPictureScreen(imagePath: image.path),
+              ),
+            );
+          } catch (e) {
+            throw Exception("Error occured while taking picture");
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+      body: FutureBuilder(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/ui/widget/display_picture_screen.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
